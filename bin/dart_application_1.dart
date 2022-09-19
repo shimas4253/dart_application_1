a3ecf13973dc528220b4374c8c4e7c3e6d6a26e2
shimas4253 <muhammedshimas123@gmail.com>
2022-09-19 15:59:34 +0530

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+import 'package:test/expect.dart';
+
+void main() {
+  print('yhh');
+
+  String name ='shimas';
+  int age=20;
+  double mark=8.8;
+  String mail='muhammedshimas@gmaio.com';
+
+print('my name is $name');
+print('i am $age years old');
+stdout.write('mark percentage is$mark');
+stdout.write('email id is$mail');
+}
